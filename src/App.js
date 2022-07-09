fd8e87a2713e5047e55b9c4d5ffa17453343de1a
Austin <austinpaul606@gmail.com>
2022-07-10 00:17:57 +0100

    edited ffor mobile view

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navigation from "./component/Navigation";
 import Projects from "./component/Projects";
 import StickyNav from "./component/StickyNav";
 import AOS from "aos";
-import "aos/dist/aos.css";
+// import "aos/dist/aos.css";
 import { useEffect } from "react";
 
 function App() {
