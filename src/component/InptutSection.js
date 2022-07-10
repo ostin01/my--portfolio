cd8b5a95d2400f5c348feb05aca0c2963563f33a
Austin <austinpaul606@gmail.com>
2022-07-10 13:01:01 +0100

    activated text message box

--- src/component/InptutSection.js
+++ src/component/InptutSection.js
@@ -2,10 +2,33 @@ import PersonIcon from "@mui/icons-material/Person";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import SendIcon from "@mui/icons-material/Send";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 const InputSection = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_4fwet6e",
+        "template_lxwj55m",
+        form.current,
+        "pFtDAShwNacKCRm2q"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
   return (
     <div data-aos="fade-left" className="inputsection__container">
-      <form method="POST">
+      <form ref={form} onSubmit={sendEmail}>
         <div className="name">
           <h4>
             <PersonIcon />
@@ -30,7 +53,7 @@ const InputSection = () => {
             <ChatBubbleOutlineIcon />
             message
           </h4>
-          <textarea rows={6} required></textarea>
+          <textarea name="message" rows={6} required></textarea>
         </div>
         <div className="submit">
           <button type="submit">
