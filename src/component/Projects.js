406580b33a7cc0e744cdfe59d60f030f02400c3a
Austin <austinpaul606@gmail.com>
2024-11-05 08:50:36 +0100

    updated the project details

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -8,10 +8,11 @@ const Projects = () => {
         <div>
           <h1>Wayg</h1>
           <p>
-            Volomn seeks to provide Nigerian techies as a service to both
-            foreign and local clients, clients can hire either some of, or all
-            of the following roles through Volomn: Product managers, software
-            engineers, designers, data analysts, etc.
+            WAYG is a digital platform that allows users to buy, share, and
+            redeem waygcards tied to specific retailers. Users can purchase
+            digital waygcards from their favorite stores and receive a unique
+            redeemable code that can be shared with friends or family creating a
+            convenient and flexible gift-giving experience.
           </p>
           <a
             href="https://getwayg.com/"
